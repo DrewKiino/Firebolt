dc4deb72671ed39a7440a7317da628cb643004f4
drewkiino <andrewaquino118@gmail.com>
2020-04-22 16:26:02 -0700

    changes

--- Package.swift
+++ Package.swift
@@ -10,6 +10,10 @@ let package = Package(
             name: "Firebolt",
             targets: ["Firebolt"])
     ],
+    platforms: [
+        // Add support for all platforms starting from a specific version.
+        .iOS(.v11),
+    ]
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
