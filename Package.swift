6bc0e2572a4f02f1a0ee1bc670af263764c0e5c4
Andrew Aquino <andrewaquino118@gmail.com>
2021-03-16 13:05:01 -0700

    specify platforms

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "Firebolt",
+    platforms: [
+        .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macOS(.v10_14), 
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
